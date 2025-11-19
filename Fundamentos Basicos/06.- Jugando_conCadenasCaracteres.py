2d1fcd96c401d82443945e1da8cd160b28d08634
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2025-11-18 22:28:55 -0600

    Jugando con las cadenas de caracteres

--- Fundamentos Basicos/06.- Jugando_conCadenasCaracteres.py
+++ Fundamentos Basicos/06.- Jugando_conCadenasCaracteres.py
@@ -1,11 +1,14 @@
-# ***** JUGANDO CON CADENAS DE CARACTERES *****
+#------------------------------------------------------------------------------------
+#                   ***** JUGANDO CON CADENAS DE CARACTERES *****
 # El presente codigo implementa la separacion de una sola cadena de caracteres a dos
 # y almacenandolas en dos variables diferentes
 # al mismo tiempo en que la cadena original se invierte el orden los caracteres
+#------------------------------------------------------------------------------------
+
 
 Mensaje = "Aprendiendo Python" #Cadena de caracteres original
 #Division del "Mensaje" en dos variables diferentes 
-Mensaje1 = Mensaje[:11] # Mensaje[:11] especifica que vas a tomar los caracteres de Mensaje desde que inicia hasta el caracter 11
+Mensaje1 = Mensaje[:11] 
 Mensaje2 = Mensaje[12:] # Especifica que le va asignar a Mensaje2 partiendo desde el caracter 12 hasta el final
 Invertido = Mensaje[::-1] # Asignas todo el contenido de mensaje pero -1 (al reves)
 
