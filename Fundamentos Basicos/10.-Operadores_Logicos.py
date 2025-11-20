3a22731558093926fba88147268da6a4bb3499cf
CesarMirandaAlberto <cesarmialberto04@gmail.com>
2025-11-19 22:19:35 -0600

    Operadores Lógicos

--- Fundamentos Basicos/10.-Operadores_Logicos.py
+++ Fundamentos Basicos/10.-Operadores_Logicos.py
@@ -1,10 +1,11 @@
+#--------------------------------------------------------------------------------------
 #       **** OPERADORES LOGICOS ****
-# Este código implementa los operadores lógicos
+# Este código implementa los operadores lógicos 
 # El código simula la lectura de un sensor de temperatura y humedad en un inverdero
 # donde si la temperatura y humedad cumplen con parametros determinados se activa el
 # ventilador en caso contrario no se enciende
 # nota: Para el ventilador True= Encendido False: Apagado
-
+#--------------------------------------------------------------------------------------
 # Definición de constantes
 VENTILADOR = False
 CALIENTE = 35
